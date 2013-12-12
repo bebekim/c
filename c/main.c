c149abfa059946b0cd1c30eb63b6b67c2f491489
bebekim <yh.kim@outlook.com>
2013-12-13 04:40:00 +0900

    Initial Commit

---
+++ c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  c
+//
+//  Created by 김영하 on 12/13/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
