5ad3a8e03db4607eb9e1f8ae997d2ddabcb5938d
bebekim <yh.kim@outlook.com>
2013-12-21 14:07:51 +0900

    finishing up snail-like array2d exercise

--- exercises/array2dsnail.c
+++ exercises/array2dsnail.c
@@ -21,12 +21,12 @@
     //일단은 array size = 5
 
 #include <stdio.h>
-#define N 5
+#define N 6
 
-void shellcheck(int (*ptr)[N], int *shell); //checks to see how many shells are to be omitted
-void showarr(int (*ptr)[N]); //show all the cell values of array2d
-void writehori (int (*ptr)[N], int *ctr, int *shell); //a function to write cell values of array2d
-void copyarr(int (*ptread)[N], int (*ptcopy)[N]); //a function within
+int checkarr(int (*ptr)[N], int *len);
+void showarr(int (*ptr)[N], int *len);
+void copyarr(int (*ptread)[N], int (*ptcopy)[N]);
+void writehori (int (*ptr)[N], int *ctr, int *len);
 void anticlockarr(int (*ptr)[N]);
 
 int main(int argc, const char * argv[])
@@ -37,57 +37,58 @@ int main(int argc, const char * argv[])
     printf("Enter the size of the 2d array : ");
     scanf("%d", pci);
     */
+
+        //keep track of length of array
+    int n = N;
+    int *len = &n;
     
         //I do not know how to allocate memory dynamically at this point. Will work on that later.
     int array2d[N][N] = {0}; //go with array2d of constant size
     int (*ptr)[N] = array2d;
-        //keep track of how many outer arrays need to be omitted
-    int s = 0;
-    int* shell = &s;
+    
         //counting how many cells (of array2d) are filled
     int counter = 0;
     int *ctr = &counter;
 
-        //check to see if array2d should be 1) shell value unaltered or 2) shell value altered
+        //check to see if array2d should be 1) rotated or 2) size reduced
     
-    shellcheck(ptr, shell);
-    writehori(ptr, ctr, shell);   //write to a array2d of varied size
+        //if 1) rotate array2d anti clockwise
     anticlockarr(ptr);
-    showarr(ptr);
+    writehori(ptr, ctr, len);   //write to a array2d of varied size
+
+        //if 2), reduce array2d size by two
+
+        anticlockarr(ptr);
     
-        //there should be a process to check if everything is complete
+
+    
+    showarr(ptr, len);
     return 0;
+    
 }
 
-void shellcheck(int (*ptr)[N], int *shell)
+int checkarr(int (*ptr)[N], int *len)
 {
-    int s = *shell;
-    if (ptr[s][s] != 0)
-        (*shell)+=1;
-    else //there should be a process to check if everything is complete
-        ;
-
-}
-
-void writehori (int (*ptr)[N], int *ctr, int *shell)
-{
-    int s = *shell;
-        //even works, odd doesn't when s == N-1-s
-    if (s == (N-1)-s)
-        ptr[s][s] = (*ctr)+1;
+    int sum = 0;
+    for (int i = 0; i < *len; i++)
+        sum += ptr[0][i];
+    if (sum ==0)
+        return 0;
     else
-        {
-            for (int i = s; i < (N-1)-s; i++)
-                {
-                    ptr[s][i] = (*ctr) + 1;
-                *ctr += 1;
-                }
-        }
+        return 1;
+}
+
+void writehori (int (*ptr)[N], int *ctr, int *len)
+{
+    int n = *len;
+    int i = (*ctr) % n;
+    for (i = 0; i < n-1; i++)
+            ptr[0][i] = ((*ctr)++) + 1;
 }
 
 
     //display array element one by one
-void showarr(int (*ptr)[N])
+void showarr(int (*ptr)[N], int *len)
 {
     int i, j;
     for (i = 0; i < N; i++)
