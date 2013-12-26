4ee6999cb50ea7779dd5739057b9d604c0a4942c
bebekim <yh.kim@outlook.com>
2013-12-27 03:50:31 +0900

    fgets includes \n

--- c/main.c
+++ c/main.c
@@ -4,36 +4,26 @@
 //
 //  Created by 김영하 on 12/21/13.
 //  Copyright (c) 2013 student. All rights reserved.
-//  int puts(const char *s);    //automatic follow up with \n
-//  int fputs(const char *s, FILE * stream);    //does not follow up with \n and thus, require manual input
-//  char * gets(char *s);
-//  char * fgets(char *s, int n, FILE *stream);
 
 #include <stdio.h>
+#include <string.h>
 
-void ClearLineFromReadBuffer (void);
+void RemoveBSN (char str[])
+{
+    unsigned long len = strlen(str);
+    str[len-1] = 0; //removing \n out of string
+}
 
 int main(int argc, const char * argv[])
 {
-    char perID[7];
-    char name[10];
+    char str[100];
+    printf("문자열 입력 : ");
+    fgets(str, sizeof(str), stdin);
+    printf("길이 : %lu, 내용 : %s\n", strlen(str), str);    //fgets에는 \n이 딸려있기 때문에 개행이 이뤄진다
     
-    fputs("주민번호 앞 6자리 입력 : ", stdout);  //remember, fputs automatically follows up with by \n
-    fgets (perID, sizeof(perID), stdin);    //accepts 6 = 7-1(=\n) characters. user's \n enter is
-    ClearLineFromReadBuffer();  //this clears the input buffer
-    
-    fputs("이름 입력 : ", stdout);  //stdout stream still contains the remaining \n single character
-    fgets(name, sizeof(name), stdin);   //this is tored in name, sizeof(1), stdin
-    
-    printf("주민번호 : %s \n", perID);
-    printf("이름 : %s\n", name);
+    RemoveBSN(str);
+    printf("길이 : %lu, 내용 : %s\n", strlen(str), str);    //여기에는 의도적으로 RemoveBSN 통해 \n을 떨궈냈기 때문에 개행이 이뤄지지 않는다
+    printf("The end \n");
     
     return 0;
-}
-
-void ClearLineFromReadBuffer (void)
-{
-    
-    while (getchar()!='\n') // 이는 \n 만날 때까지 입력버퍼에 저장된 문자들을 char으로 계속 읽어낸다. 별도로 저장하지는 않는다.
-        ;
 }
