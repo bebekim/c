d1766d1c0d057f70fcceb8f696c8027f4693ab38
bebekim <yh.kim@outlook.com>
2014-01-21 01:05:55 +0900

    file and stream

--- c/main.c
+++ c/main.c
@@ -4,50 +4,25 @@
 //
 //  Created by 김영하 on 12/21/13.
 //  Copyright (c) 2013 student. All rights reserved.
-//  #include <string.h>
-//  ch 23 : 구조체와 사용자 정의 자료형2
-
 
 #include <stdio.h>
 
-typedef struct point {
-    int xpos;
-    int ypos;
-} Point;
-
-typedef struct rectangle {
-    struct point Point1;
-    struct point Point2;
-} Rectangle;
-
-void CalcArea (Rectangle shape);
-void ShowCorners (Rectangle shape);
-
 int main(int argc, const char * argv[])
 {
-    Point topleft = {0, 0};
-    Point bottomright = {100, 100};
+    FILE *fp = fopen ("/Users/goldenfermi/BTSync/Programming/c/exercises/ch24_data.txt", "wt"); //fopen함수가 스트림을 형성하는 함수, fclose함수는 스트림을 해제하는 함수. 즉, fopen 함수가 파일을 개방하면, fclose함수는 파일을 닫는다.
+                                                                                                //wt는 출력 스트림의 형성
+                                                                                                //data.txt 와 스트림을 형성하되, wt모드로 스트림을 형성해라.
     
-    Rectangle shape1;
-    Rectangle *shptr = &shape1;
-    shptr->Point1 = topleft;
-    shptr->Point2 = bottomright;
+    if (fp == NULL)
+        {
+        puts ("파일오픈 실패!");
+        return -1;  //비정상적인 종료를 의미하기 위해서 -1을 반환
+        }
     
-    CalcArea(shape1);
-    ShowCorners(shape1);
-    
+    fputc ('A', fp);
+    fputc ('B', fp);
+    fputc ('C', fp);
+    fclose(fp); //스트림의 종료
     return 0;
 }
 
-void CalcArea (Rectangle shape)
-{
-    printf("Area of rectangle : %d \n", (shape.Point2.xpos - shape.Point1.xpos) * (shape.Point2.ypos - shape.Point1.ypos));
-    
-}
-
-void ShowCorners (Rectangle shape)
-{
-    printf("The corners of the rectangle\n");
-    printf("Top left : [%d, %d] \n", shape.Point1.xpos, shape.Point1.ypos);
-    printf("Bottom Right : [%d, %d]\n", shape.Point2.xpos, shape.Point2.ypos);
-}
